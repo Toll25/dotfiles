78490eddc6f80955184f362bdc5a18f702e953c1
Toll <paulipanther.hoesch@gmail.com>
2024-07-18 18:36:41 +0200

    added qt themes (qt6 bit broken) and cursors

--- stow/.config/fish/functions/cd-config.fish
+++ stow/.config/fish/functions/cd-config.fish
@@ -1,4 +1,4 @@
-function cdconfig --wraps='cd /home/elena/Documents/dotfiles' --description 'alias cdconfig=cd /home/elena/Documents/dotfiles'
-  cd /home/elena/Documents/dotfiles $argv
-        
+function cd-config --wraps='cd /home/elena/Documents/dotfiles' --description 'alias cdconfig=cd /home/elena/Documents/dotfiles'
+    cd /home/elena/Documents/dotfiles $argv
+
 end
--- stow/.config/fish/functions/edit-config.fish
+++ stow/.config/fish/functions/edit-config.fish
@@ -1,4 +1,4 @@
-function editconfig --wraps='nvim /home/elena/Documents/dotfiles/' --description 'alias editconfig=nvim /home/elena/Documents/dotfiles/'
-  nvim /home/elena/Documents/dotfiles/ $argv
-        
+function edit-config --wraps='nvim /home/elena/Documents/dotfiles/' --description 'alias editconfig=nvim /home/elena/Documents/dotfiles/'
+    nvim /home/elena/Documents/dotfiles/ $argv
+
 end
