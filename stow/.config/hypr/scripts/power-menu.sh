2e9f1064eb8c3908da3571495417dec3f16e261e
Toll <paulipanther.hoesch@gmail.com>
2024-07-20 00:18:28 +0200

    added swaync config, adjusted powermenu

--- stow/.config/hypr/scripts/power-menu.sh
+++ stow/.config/hypr/scripts/power-menu.sh
@@ -1,29 +1,28 @@
-#!/bin/bash
+#!/bin/env bash
 
 # Define the options
 options="⏻ Shutdown\n Reboot\n Lock\n󰒲 Suspend\n󰍃 Logout"
 
 # Show the options using Fuzzel in dmenu mode and capture the selected option
-selected=$(echo -e $options | fuzzel --lines=5 --width=15 --dmenu)
+selected=$(echo -e "$options" | fuzzel --lines=5 --width=15 --dmenu)
 
 # Perform action based on selected option
 case "$selected" in
-    Shutdown)
+    "⏻ Shutdown")
         systemctl poweroff
         ;;
-    Reboot)
+    " Reboot")
         systemctl reboot
         ;;
-    Lock)
+    " Lock")
         hyprlock
         ;;
-    Suspend)
+    "󰒲 Suspend")
         systemctl suspend
         ;;
-    Logout)
-		hyprctl dispatch exit 1
+    "󰍃 Logout")
+        hyprctl dispatch exit 1
         ;;
     *)
         ;;
 esac
-
