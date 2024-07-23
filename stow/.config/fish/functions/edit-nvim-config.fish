0553ad68efaee70b52be0a4eb4024d8f5a486fee
Toll <paulipanther.hoesch@gmail.com>
2024-07-23 20:06:34 +0200

    updated bin path and added new edit alias

---
+++ stow/.config/fish/functions/edit-nvim-config.fish
@@ -0,0 +1,4 @@
+function edit-nvim-config --wraps='nvim $HOME/.dotfiles/stow/.config/nvim' --description 'alias edit-nvim-config=nvim $HOME/.dotfiles/stow/.config/nvim'
+  nvim $HOME/.dotfiles/stow/.config/nvim $argv
+        
+end
