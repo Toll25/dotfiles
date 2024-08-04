0b0516584c382447dd6128449ea0d664f94cdeb7
Toll <paulipanther.hoesch@gmail.com>
2024-08-05 00:38:56 +0200

    added open-notes alias

---
+++ stow/.config/fish/functions/open-notes.fish
@@ -0,0 +1,4 @@
+function open-notes --wraps=nvim\ -c\ \'Neorg\ workspace\ notes\' --description alias\ open-notes=nvim\ -c\ \'Neorg\ workspace\ notes\'
+  nvim -c 'Neorg workspace notes' $argv
+        
+end
