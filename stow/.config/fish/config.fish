07d3215224a4a074a7f65770486fb9d2cd9d60cd
Toll <toll25@proton.me>
2024-08-09 02:40:26 +0200

    updated fish settings

--- stow/.config/fish/config.fish
+++ stow/.config/fish/config.fish
@@ -1,11 +1,12 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    atuin init fish --disable-up-arrow | source
+    bind \e\[1\;5A _atuin_search # bind CTRL+<UP> to atuin search
+    starship init fish | source
+    set -U fish_greeting
+    zoxide init fish | source
 end
 #if status is-login
 #    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
 #        exec Hyprland
 #    end
 #end
-starship init fish | source
-set -U fish_greeting
-jump shell fish | source
