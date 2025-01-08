3101ac84889f5a0de122f285dad742432e8c0a4b
Toll <toll25@proton.me>
2025-01-08 19:11:25 +0100

    added mason

---
+++ stow/dot-config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
+	{ "williamboman/mason-lspconfig.nvim", opts = {} },
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"lua_ls",
+				"shellharden",
+				"beautysh",
+				"black",
+				"prettierd",
+				"bashls",
+				-- "hyprls",
+				"jdtls",
+				"pylsp",
+				"tailwindcss",
+				"svelte",
+				"taplo",
+				"ts_ls",
+				"jsonls",
+				"html",
+				"cssls",
+				"rust-analyzer",
+			},
+		},
+	},
+}
